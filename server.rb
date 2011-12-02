2e2787ca52649e9d65cd7a3f1d882efdb9b34ebb
Dims <git@dims.kz>
2011-12-02 14:15:52 +0400

    Fixed "free RAM" value

--- server.rb
+++ server.rb
@@ -5,10 +5,10 @@ free_ram = `free`;
 uptime = `uptime`;
 running_processes = `ps -ef`;
 
-body = "<h3>Uptime</h3><p>#{uptime}</p>";
-body += "<h3>Free Storage Space</h3><p>#{free_storage_space}</p>"
+body = "<h3>Uptime</h3><p><pre><code>#{uptime}</code></pre></p>";
+body += "<h3>Free Storage Space</h3><p><pre><code>#{free_storage_space}</code></pre></p>"
 body += "<h3>Free RAM &amp; Running processes</h3>"
-body += "<p>#{free_storage_space}</p><p><pre><code>#{running_processes}</code></pre></p>"
+body += "<p><pre><code>#{free_ram}</code></pre></p><p><pre><code>#{running_processes}</code></pre></p>"
 
 subject = "Dims Server Monitoring for #{Time.now.utc}"
 send_email('dsemenyuk@gmail.com', :subject => subject, :body => body, :content_type => 'text/html')
